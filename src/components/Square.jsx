0819ed20ba3caf2fc0dc91a3d677de53a5af60c5
lmbruno <bmlopez93@gmail.com>
2024-07-30 20:55:50 -0300

    winner modal comp / update square component

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,4 +1,12 @@
 export function Square({ children, isSelected, updateBoard, index }) {
   const className = `square ${isSelected ? "is-selected" : ""}`;
-  return <div className={className}>{children}</div>;
+
+  const handleClick = () => {
+    updateBoard(index);
+  };
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  );
 }
